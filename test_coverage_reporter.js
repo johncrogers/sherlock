90503ddf13b0b685da31d47bdfdc02972aadb96a
John Rogers <john.clinton.rogers@gmail.com>
2019-04-27 12:26:10 -0500

    Improve coverage logging

--- test_coverage_reporter.js
+++ test_coverage_reporter.js
@@ -1,12 +1,17 @@
-const cTable = require("console.table");
+require("console.table");
 const { readdirSync } = require("fs");
 let helpersPath = "./helpers";
-let testCoverage = {};
+let testCoverage = [["Helper:", "Test File:"]];
 readdirSync(helpersPath, { encoding: "utf8" }).forEach(helper => {
   if (helper !== "index.js") {
-    testCoverage[helper] = readdirSync(`${helpersPath}/${helper}`, {
-      encoding: "utf8"
-    }).includes("test.js");
+    testCoverage.push([
+      helper,
+      readdirSync(`${helpersPath}/${helper}`, {
+        encoding: "utf8"
+      }).includes("test.js")
+    ]);
   }
 });
-console.table(testCoverage);
+console.group("Test Coverage:");
+console.table(testCoverage[0], testCoverage.slice(1));
+console.groupEnd();
